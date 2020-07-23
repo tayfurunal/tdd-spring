aed7c9368c854be8ae59b62f9a5fb83ff7978d09
tayfurunal <mtayfurunal@gmail.com>
2020-07-23 14:59:20 +0300

    configure spring security

---
+++ src/main/java/com/tayfurunal/tddspring/configuration/AuthUserService.java
@@ -0,0 +1,27 @@
+package com.tayfurunal.tddspring.configuration;
+
+import com.tayfurunal.tddspring.user.User;
+import com.tayfurunal.tddspring.user.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthUserService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return (UserDetails) user;
+    }
+
+}
